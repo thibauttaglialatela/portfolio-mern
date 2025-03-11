e24b951602a698cf22571b269743d1067d4a2876
thibauttaglialatela <thibauttaglialatela@gmail.com>
2025-03-11 11:16:14 +0100

    📦 NEW: create the models

---
+++ Server/src/models/Skill.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const skillSchema = new Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    logo_url: { type: String, required: true },
+    logo_alt: { type: String, required: true },
+    category: { type: String, enum: ["Frontend", "Backend", "Project"], required: true },
+    level: { type: String, enum: ["Débutant", "Intermédiaire", "Avancé"] },
+  },
+  { timestamps: true }
+);
+
+const Skill = mongoose.model("Skill", skillSchema);
+
+module.exports = Skill;
