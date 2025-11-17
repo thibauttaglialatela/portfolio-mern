98dfeafa568e5f9d86bcbba5035bc25d9e072049
thibauttaglialatela <thibauttaglialatela@gmail.com>
2025-11-17 17:23:48 +0100

    refactor: projects page

--- Client/src/pages/Projects.jsx
+++ Client/src/pages/Projects.jsx
@@ -1,34 +1,8 @@
-import { useState, useEffect } from "react";
+import useJsonData from '../utils/hooks/useJsonData.jsx'
 import { ProjectCard } from "../components/ProjectCard";
 
 const Projects = () => {
-  const [projectData, setProjectData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  const BASE_URL = import.meta.env.VITE_BASE_URL_API;
-
-  useEffect(() => {
-    const fetchProjectData = async () => {
-      try {
-        const response = await fetch(`${BASE_URL}api/portfolio/projects`);
-
-        if (!response.ok) {
-          throw new Error("Erreur lors de la récupération des données");
-        }
-
-        const data = await response.json();
-        setProjectData(data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchProjectData();
-  }, [BASE_URL]);
-
-  if (loading) return <p>Chargement des données...</p>;
+  const { data: projectData, error} = useJsonData('projects.json')
   if (error) return <p>Erreur: {error}</p>;
 
   return (
@@ -39,10 +13,10 @@ const Projects = () => {
       <section className="grid grid-cols-1 lg:grid-cols-3 gap-4 p-4 place-items-center my-4">
         {projectData.map((project) => (
           <ProjectCard
-            key={project._id}
+            key={project.id}
             name={project.name}
             content={project.content}
-            imageUrl={project && project.image_url ? `${BASE_URL}images/${project.image_url}` : 'https://picsum/300/200'}
+            imageUrl={project && project.image_url ? `images/${project.image_url}` : 'https://picsum/300/200'}
             imageAlt={project.image_alt}
             githubLink={project.github_link}
             liveDemoLink={project.live_demo_link}
