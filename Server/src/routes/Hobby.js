97e4107a0ee293ed2e577c92d385b1123b17b840
thibauttaglialatela <thibauttaglialatela@gmail.com>
2025-11-18 10:04:47 +0100

    refactor: :coffin: remove the server folder

--- Server/src/routes/Hobby.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getAllHobbies } = require('../controllers/HobbyController.js');
-
-const hobbyRoute = express.Router();
-
-hobbyRoute.get('/hobbies', getAllHobbies);
-
-module.exports = hobbyRoute;
--- Server/src/routes/Project.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getAllProjects } = require("../controllers/ProjectController.js")
-const projectRoute = express.Router();
-
-
-//Get all projects
-projectRoute.get('/', getAllProjects);
-
-module.exports = projectRoute ;
